8781b7707ef483e8ed895951995647eee83ed24f
Andreas Linneweber <al@linneweber.org>
2021-11-08 07:56:14 +0000

    Initial reduce to core platform playbook

--- 01_setup_acn/packer/scripts/ubuntu/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eux
-export DEBIAN_FRONTEND=noninteractive
-
-# Update the package list
-apt-get -y update;
-
-# Upgrade all installed packages incl. kernel and kernel headers
-apt-get -y dist-upgrade -o Dpkg::Options::="--force-confnew";
